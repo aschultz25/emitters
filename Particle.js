1e5f69b4550285b434965263cc3b0aa67efafce5
aschultz25 <aschultz25@avenues.org>
2025-05-14 08:11:50 -0400

    Update Particle shape.js

--- Particle.js
+++ Particle.js
@@ -8,24 +8,37 @@ class Particle {
     this.size = random(3, 8)
     this.vel = createVector(random(-1.5, 1.5), random(-2, 0))
   }
-
+  
   applyForce(force) {
-    this.acc.add(force);
+    this.acc.add(force)
   }
 
   update() {
-    this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-    this.lifespan -= 2;
+    this.vel.add(this.acc)
+    this.pos.add(this.vel)
+    this.acc.mult(0)
+    this.lifespan -= 2
   }
 
   draw() {
-    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.lifespan);
+    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.lifespan)
     ellipse(this.pos.x, this.pos.y, this.size)
   }
 
   isDead() {
-    return this.lifespan <= 0;
+    return this.lifespan <= 0
+  }
+}
+
+class SquareParticle extends Particle {
+  constructor(x, y) {
+    super(x, y)
+    this.size = random(5, 10)
+    this.color = color(random(255), random(255), random(255))
+  }
+
+  draw() {
+    fill(this.color.levels[0], this.color.levels[1], this.color.levels[2], this.lifespan)
+    ellipse(this.pos.x, this.pos.y, this.size, this.size)
   }
 }
